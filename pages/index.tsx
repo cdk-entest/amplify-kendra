f78e185980192b785d8d74ce577ddf90c253d1f4
entest <hai@entest.io>
2022-09-12 12:31:54 +0000

    update ui search interface

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import { useState } from "react";
-import { Flex, Text, chakra, Center } from "@chakra-ui/react";
+import { Flex, Text, chakra, Center, Spinner } from "@chakra-ui/react";
 import * as React from "react";
 import Link from "next/link";
 import { SearchIcon } from "@chakra-ui/icons";
@@ -10,19 +10,29 @@ const ListItem = ({ items }: { items: [] }) => {
   return (
     <div>
       {items.map((item, idx) => (
-        <Flex
-          direction={"column"}
-          key={idx}
-          backgroundColor={"green.100"}
-          margin={"auto"}
-          marginTop={"10px"}
-          padding={"10px"}
-        >
-          <Text>{item["DocumentId"]}</Text>
-          <Text>{item["DocumentTitle"]["Text"]}</Text>
-          <Text>{item["DocumentURI"]}</Text>
-          <Text>{item["DocumentExcerpt"]["Text"]}</Text>
-        </Flex>
+        <Link href={item["DocumentId"]} passHref>
+          <a target={"_blank"}>
+            <Flex
+              direction={"column"}
+              key={idx}
+              // backgroundColor={"green.100"}
+              margin={"auto"}
+              marginTop={"10px"}
+              padding={"10px"}
+            >
+              <Text>{item["DocumentId"]}</Text>
+              <Text
+                fontWeight={"bold"}
+                color="blue.700"
+                _hover={{ textDecoration: "underline" }}
+              >
+                {item["DocumentTitle"]["Text"]}
+              </Text>
+              <Text>{item["DocumentURI"]}</Text>
+              <Text>{item["DocumentExcerpt"]["Text"]}</Text>
+            </Flex>
+          </a>
+        </Link>
       ))}
     </div>
   );
@@ -33,13 +43,16 @@ const ACTION_KEY_APPLE = ["⌘", "Command"];
 
 const Home: NextPage = () => {
   const [items, setItems] = useState<any>([]);
-  const [query, setQuery] = useState("xxx");
+  const [query, setQuery] = useState("");
+  const [spinning, setSpinning] = useState(false);
   const eventRef = React.useRef<"mouse" | "keyboard">(null);
 
   const callSearch = async (query: string) => {
     if (query) {
+      setSpinning(true);
       const result = await search(query);
       setItems(result);
+      setSpinning(false);
     }
   };
 
@@ -77,6 +90,12 @@ const Home: NextPage = () => {
         ></chakra.input>
         <Center pos={"absolute"} left={7} h={"68px"}>
           <SearchIcon color={"teal.500"} boxSize={"20px"}></SearchIcon>
+        </Center>
+        <Center pos={"absolute"} right={7} h={"68px"}>
+          <Spinner
+            color="green"
+            display={spinning ? "block" : "None"}
+          ></Spinner>
         </Center>
       </Flex>
       {items.length > 0 && <ListItem items={items}></ListItem>}
