8aa0b610d22dac79344774efd17349e1d5a7952e
entest <hai@entest.io>
2022-09-12 13:27:30 +0000

    remove amplify config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Amplify } from "aws-amplify";
-import awsconfig from "./../src/aws-exports";
+// import { Amplify } from "aws-amplify";
+// import awsconfig from "./../src/aws-exports";
 import { ChakraProvider } from "@chakra-ui/react";
 
-try {
-  Amplify.configure(awsconfig);
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   Amplify.configure(awsconfig);
+// } catch (error) {
+//   console.log(error);
+// }
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
